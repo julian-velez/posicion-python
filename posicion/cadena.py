6851a062f58b72b65b8620149b6555220ad8110b
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-06-19 18:40:48 -0500

    posicion-python
    posicion-python

---
+++ posicion/cadena.py
@@ -0,0 +1,12 @@
+# Creamos una variable llamada 'texto' y le asignamos el valor de una cadena (string)
+texto = "julian velez G"
+
+# Usamos la función print() para mostrar en pantalla un carácter específico de la cadena
+# texto[2] significa: acceder al carácter que está en la posición 2 de la cadena
+# IMPORTANTE: en Python (y en la mayoría de lenguajes de programación), la posición de los caracteres empieza en 0
+# Es decir:
+# texto[0] -> 'j'
+# texto[1] -> 'u'
+# texto[2] -> 'l'  --> este es el que se mostrará
+
+print(texto[2])
